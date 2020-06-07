77a6e46ab1c04226be571e3b5197e72e0d6f068a
Neeraj Rattehalli <neeraj.rattehalli@gmail.com>
2020-06-06 17:56:58 -0700

    Added escape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pynput.keyboard import Key, Controller
 import time
+import sys
 
 keyboard = Controller()
 
@@ -7,10 +8,19 @@ text = input("What do you want to spam? ")
 num = input("How many times? ")
 wait = input("How much time do you want to wait between each turn? ")
 
-print("You have five seconds to go to the place where you want to start typing.\n You can quit by pressing Ctrl + C in the terminal.")
+print("You have five seconds to go to the place where you want to start typing.\n You can quit by pressing escape.")
 
 time.sleep(5)
-for i in range(int(num)):
-    keyboard.type(text)
-    keyboard.press(Key.enter)
-    time.sleep(int(wait))
+
+while(True):
+    try:
+        for i in range(int(num)):
+            keyboard.type(text)
+            keyboard.press(Key.enter)
+            time.sleep(int(wait))
+
+            if(keyboard.is_pressed('Esc')):
+                sys.exit(0)
+    except:
+        break
+        
