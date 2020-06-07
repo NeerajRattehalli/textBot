a5b61e16869bc5eea32647fb323acc8cd3d9e980
Neeraj Rattehalli <neeraj.rattehalli@gmail.com>
2020-06-06 17:47:40 -0700

    fixed small errors

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ keyboard = Controller()
 
 text = input("What do you want to spam? ")
 num = input("How many times? ")
-wait = input("How much time do you want to wait between each turn?")
+wait = input("How much time do you want to wait between each turn? ")
 
 print("You have five seconds to go to the place where you want to start typing.\n You can quit by pressing Ctrl + C in the terminal.")
 
 time.sleep(5)
-for i in range(num):
+for i in range(int(num)):
     keyboard.type(text)
     keyboard.press(Key.enter)
-    time.sleep(time)
+    time.sleep(int(wait))
